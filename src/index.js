f3f50e796d864e0404b8cfa9d906a827084e7ed7
Pranavb <pranavb@IN-Pranavb.local>
2019-03-27 12:53:58 +0530

    Evaluated name in JSX

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 
+const format = function (user) {
+    return user.firstName + " " + user.lastName;
+}
+
+
+const user = {
+    firstName: 'Levi',
+    lastName: 'Ackerman'
+};
+
 class Hello extends Component{
     render(){
-        return <div>Hello {this.props.name}</div>;
+        const name = this.props.user;
+        if(name){
+            return <div>Hello {format(name)}</div>;
+        }
+        return <div>Hello Patel</div>
     };
 }
 
 
-ReactDOM.render(<Hello name="Pranav"/>,document.getElementById('root'));
+ReactDOM.render(<Hello/>,document.getElementById('root'));
