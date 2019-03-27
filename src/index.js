2bea3c5ad407482a8eaafb2dbd4718a054e4f81d
Pranavb <pranavb@IN-Pranavb.local>
2019-03-27 12:50:45 +0530

    passed name as a prop

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 
 class Hello extends Component{
     render(){
-        return <div>Hello World</div>;
+        return <div>Hello {this.props.name}</div>;
     };
 }
 
 
-ReactDOM.render(<Hello/>,document.getElementById('root'));
+ReactDOM.render(<Hello name="Pranav"/>,document.getElementById('root'));
